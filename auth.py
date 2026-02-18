d86b892c9c1a30f8af94c4de9c525a96983bef84
Rajiv Boorla <rajivboorla103@gmail.com>
2026-02-18 21:29:08 +0530

    changes modified in main file & adding requirements file

---
+++ auth.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from database import config
+
+SECRET_KEY = config.get("SECRET_KEY", "supersecret")
+ALGORITHM = config.get("SECRET_KEY", "algorithm")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire, "type": "access"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
